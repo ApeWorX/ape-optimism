2f1095970d6b228c0eed4b713037112b477f9c5e
unparalleled-js <yingthi@live.com>
2022-10-07 15:26:20 -0500

    fix: network fixes

--- ape_optimism/ecosystem.py
+++ ape_optimism/ecosystem.py
@@ -1,6 +1,9 @@
+from typing import Optional, Type, Union, cast
+
 from ape.api import TransactionAPI
 from ape.api.config import PluginConfig
 from ape.api.networks import LOCAL_NETWORK_NAME
+from ape.exceptions import ApeException
 from ape.types import TransactionSignature
 from ape_ethereum.ecosystem import Ethereum, NetworkConfig
 from ape_ethereum.transactions import StaticFeeTransaction, TransactionType
@@ -10,55 +13,52 @@ from eth_utils import add_0x_prefix, decode_hex
 NETWORKS = {
     # chain_id, network_id
     "mainnet": (10, 10),
-    "kovan": (69, 69),
     "goerli": (420, 420),
 }
 
 
+class ApeOptimismError(ApeException):
+    """
+    Raised in the ape-optimism plugin.
+    """
+
+
+def _create_network_config(
+    required_confirmations: int = 1, block_time: int = 2, **kwargs
+) -> NetworkConfig:
+    return NetworkConfig(
+        required_confirmations=required_confirmations, block_time=block_time, **kwargs
+    )  # type: ignore
+
+
 class OptimismConfig(PluginConfig):
-    mainnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=2)  # type: ignore
-    kovan: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=2)  # type: ignore
-    goerli: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=2)  # type: ignore
-    local: NetworkConfig = NetworkConfig(default_provider="test")  # type: ignore
+    mainnet: NetworkConfig = _create_network_config()
+    goerli: NetworkConfig = _create_network_config()
+    local: NetworkConfig = _create_network_config(
+        required_confirmations=0, default_provider="test", block_time=0
+    )
     default_network: str = LOCAL_NETWORK_NAME
 
 
 class Optimism(Ethereum):
     @property
     def config(self) -> OptimismConfig:  # type: ignore
-        return self.config_manager.get_config("optimism")  # type: ignore
+        return cast(OptimismConfig, self.config_manager.get_config("optimism"))
 
     def create_transaction(self, **kwargs) -> TransactionAPI:
         """
         Returns a transaction using the given constructor kwargs.
+        Overridden because does not support
+
+        **kwargs: Kwargs for the transaction class.
+
         Returns:
             :class:`~ape.api.transactions.TransactionAPI`
         """
 
-        transaction_types = {
-            TransactionType.STATIC: StaticFeeTransaction,
-        }
-
-        if "type" in kwargs:
-            type_kwarg = kwargs["type"]
-            if type_kwarg is None:
-                type_kwarg = TransactionType.STATIC.value
-            elif isinstance(type_kwarg, int):
-                type_kwarg = f"0{type_kwarg}"
-            elif isinstance(type_kwarg, bytes):
-                type_kwarg = type_kwarg.hex()
-
-            suffix = type_kwarg.replace("0x", "")
-            if len(suffix) == 1:
-                type_kwarg = f"{type_kwarg.rstrip(suffix)}0{suffix}"
-
-            version_str = add_0x_prefix(HexStr(type_kwarg))
-            version = TransactionType(version_str)
-        else:
-            version = TransactionType.STATIC
-
-        txn_class = transaction_types[version]
-        kwargs["type"] = version.value
+        transaction_type = _get_transaction_type(kwargs.get("type"))
+        kwargs["type"] = transaction_type.value
+        txn_class = _get_transaction_cls(transaction_type)
 
         if "required_confirmations" not in kwargs or kwargs["required_confirmations"] is None:
             # Attempt to use default required-confirmations from `ape-config.yaml`.
@@ -82,4 +82,32 @@ class Optimism(Ethereum):
                 s=bytes(kwargs["s"]),
             )
 
-        return txn_class(**kwargs)  # type: ignore
+        return txn_class.parse_obj(kwargs)
+
+
+def _get_transaction_type(_type: Optional[Union[int, str, bytes]]) -> TransactionType:
+    if not _type:
+        return TransactionType.STATIC
+
+    if _type is None:
+        _type = TransactionType.STATIC.value
+    elif isinstance(_type, int):
+        _type = f"0{_type}"
+    elif isinstance(_type, bytes):
+        _type = _type.hex()
+
+    suffix = _type.replace("0x", "")
+    if len(suffix) == 1:
+        _type = f"{_type.rstrip(suffix)}0{suffix}"
+
+    return TransactionType(add_0x_prefix(HexStr(_type)))
+
+
+def _get_transaction_cls(transaction_type: TransactionType) -> Type[TransactionAPI]:
+    transaction_types = {
+        TransactionType.STATIC: StaticFeeTransaction,
+    }
+    if transaction_type not in transaction_types:
+        raise ApeOptimismError(f"Transaction type '{transaction_type}' not supported.")
+
+    return transaction_types[transaction_type]
